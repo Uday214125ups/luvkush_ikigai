3d58dd9b59c8aa6c1282957525d47c33bae26d22
Uday Pratap Singh <uday214125ups@gmail.com>
2023-12-14 14:11:44 +0530

    .

---
+++ request.js
@@ -0,0 +1,14 @@
+const key = 'fecc6facaebec077d40093931b9ff6e6';
+
+const requestCity = async (city) => {
+    const baseURL = 'http://api.openweathermap.org/data/2.5/weather'
+    const query = `?q=${city}&appid=${key}`;
+
+    //make fetch call (promise call)
+    const response = await fetch(baseURL + query);
+
+    //promise data
+    const data = await response.json();
+    return data;
+
+}
